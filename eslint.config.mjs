123923d3b678d819525a6379dc485efa948a290d
gazuyawork <gazuyawork@gmail.com>
2025-05-04 17:59:35 +0900

    202505041759_backup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/display-name": "off",           // displayNameエラーを無効化
+      "@typescript-eslint/no-explicit-any": "warn", // any使用は警告のみに
+    },
+  },
 ];
+
 
 export default eslintConfig;
