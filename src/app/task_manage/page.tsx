6d3e168c15ce9d65a1fe4be2c7973575ef429590
gazuyawork <gazuyawork@gmail.com>
2025-05-06 23:09:57 +0900

    202505062309_backup

--- src/app/task_manage/page.tsx
+++ src/app/task_manage/page.tsx
@@ -28,7 +28,6 @@ interface TaskCardProps {
   onToggleUser: (id: number, user: string) => void;
   onToggleDay: (id: number, day: string) => void;
   onToggleDelete: (id: number) => void;
-  // generatePointOptions: () => React.ReactElement[];
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({
@@ -38,7 +37,6 @@ const TaskCard: React.FC<TaskCardProps> = ({
   onToggleUser,
   onToggleDay,
   onToggleDelete,
-  // generatePointOptions,
 }) => {
   const days = ['月', '火', '水', '木', '金', '土', '日'];
 
@@ -384,7 +382,6 @@ export default function TaskManagePage() {
                 onToggleUser={handleUserToggle}
                 onToggleDay={toggleDay}
                 onToggleDelete={toggleShowDelete}
-                generatePointOptions={generatePointOptions}
               />
             ))}
         </div>
