d96cb331919985d8f5471afdbf2b9a4073e9105f
k.matsumoto <k.matsumoto@leafnet.jp>
2025-05-08 12:15:01 +0900

    202505081214_backup

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -1,8 +1,6 @@
-// src/components/FooterNav.tsx
-
 'use client';
 
-import { Home, User, Puzzle, ListTodo } from 'lucide-react';
+import { Home, LayoutList, ListTodo } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 
 export default function FooterNav() {
@@ -11,22 +9,36 @@ export default function FooterNav() {
 
   const navItems = [
     { name: 'ホーム', icon: Home, href: '/home' },
-    { name: '掃除', icon: ListTodo, href: '/task' },
-    { name: 'ピース', icon: Puzzle, href: '/task_manage' },
-    { name: 'プロフィール', icon: User, href: '/profile' },
+    { name: '掃除', icon: ListTodo, href: '/task' }, // 中央に配置
+    { name: 'プロフィール', icon: LayoutList, href: '/profile' },
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200 shadow-inner px-10 pt-4 pb-8">
-      <ul className="flex justify-between">
+    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200 shadow-inner pt-4 pb-6">
+      <ul className="relative flex justify-around items-end mx-8">
         {navItems.map((item, index) => {
           const isActive = pathname === item.href;
           const Icon = item.icon;
+          const isCenter = index === 1; // 中央アイコン
+
           return (
-            <li key={index} onClick={() => router.push(item.href)} className="flex flex-col items-center cursor-pointer">
-              <div className={isActive ? 'text-[#FFCB7D]' : 'text-[#5E5E5E]'}>
-                <Icon size={32}/>
+            <li
+              key={index}
+              onClick={() => router.push(item.href)}
+              className="relative flex flex-col items-center cursor-pointer"
+            >
+            {isCenter ? (
+              <div className="relative -mt-10 z-10">
+                <div className="bg-white w-24 h-24 rounded-full flex items-center justify-center shadow-lg border-2 border-white">
+                  <Icon size={30} className={isActive ? 'text-[#FFCB7D]' : 'text-[#5E5E5E]'} />
+                </div>
               </div>
+            ) : (
+              <div className="w-16 h-16 flex items-center justify-center pb-6">
+                <Icon size={26} className={isActive ? 'text-[#FFCB7D]' : 'text-[#5E5E5E]'} />
+              </div>
+            )}
+
             </li>
           );
         })}
