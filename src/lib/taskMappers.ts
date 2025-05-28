5ac70d862b5f7142dbb80438b770bcb6f4467192
gazuyawork <gazuyawork@gmail.com>
2025-05-28 22:39:00 +0900

    202505282238_backup

--- src/lib/taskMappers.ts
+++ src/lib/taskMappers.ts
@@ -2,6 +2,7 @@
 import { DocumentData } from 'firebase/firestore';
 import { getProfileImage } from '@/hooks/useProfileImages';
 import type { Task, FirestoreTask } from '@/types/Task';
+import { dayNumberToName } from '@/lib/constants'; 
 
 export const mapFirestoreDocToTask = (doc: DocumentData): Task => {
   const data = doc.data() as FirestoreTask;
@@ -19,7 +20,7 @@ export const mapFirestoreDocToTask = (doc: DocumentData): Task => {
     completedBy: data.completedBy ?? '',
     person: user,
     image: getProfileImage(user),
-    daysOfWeek: data.daysOfWeek ?? [],
+    daysOfWeek: (data.daysOfWeek ?? []).map((code: string) => dayNumberToName[code] ?? ''), // ← 変換追加
     dates: data.dates ?? [],
     isTodo: data.isTodo ?? false,
     users: data.users ?? [],
