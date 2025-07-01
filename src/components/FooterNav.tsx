d0d0482f5412c91a690390e6e48a99f5f790e906
k.matsumoto <k.matsumoto@leafnet.jp>
2025-07-01 14:36:12 +0900

    202507011436_backup

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -10,13 +10,13 @@ type Props = {
 
 export default function FooterNav({ currentIndex, setIndex }: Props) {
   const navItems = [
-    { name: 'ホーム', icon: Home },
-    { name: 'タスク', icon: null }, // アイコン画像を使うためnullにする
+    { name: 'Home', icon: Home },
+    { name: 'Task', icon: null }, // 中央アイコンは画像
     { name: 'Todo', icon: ListTodo },
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200 shadow-inner pt-4 pb-6">
+    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200 shadow-inner pt-2 pb-6">
       <ul className="relative flex justify-around items-end mx-10">
         {navItems.map((item, index) => {
           const isActive = currentIndex === index;
@@ -27,21 +27,28 @@ export default function FooterNav({ currentIndex, setIndex }: Props) {
               onClick={() => setIndex(index)}
               className="relative flex flex-col items-center cursor-pointer"
             >
-              <div className="w-10 h-10 flex items-center justify-center pb-4">
+              <div className="flex items-center justify-center">
                 {item.icon ? (
                   <item.icon
-                    size={26}
+                    size={30}
                     className={isActive ? 'text-[#FFCB7D]' : 'text-[#5E5E5E]'}
                   />
                 ) : (
                   <Image
                     src={isActive ? '/icons/task_on.png' : '/icons/task_off.png'}
                     alt="Task Icon"
-                    width={50}
-                    height={50}
+                    width={45}
+                    height={45}
                   />
                 )}
               </div>
+              <span
+                className={`mt-0 text-xs ${
+                  isActive ? 'text-[#FFCB7D] font-semibold' : 'text-[#5E5E5E]'
+                }`}
+              >
+                {item.name}
+              </span>
             </li>
           );
         })}
