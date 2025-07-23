34e0a3443a4ae65ab189381d03d4ac8df7863f16
k.matsumoto <k.matsumoto@leafnet.jp>
2025-07-23 15:19:26 +0900

    20250723_タスクカードの日時部分の入力情報削除処理追加（iphone）_02

--- src/components/task/parts/EditTaskModal.tsx
+++ src/components/task/parts/EditTaskModal.tsx
@@ -305,7 +305,7 @@ useEffect(() => {
           const date = e.target.value;
           update('dates', [date]);
         }}
-        className="w-full border-b border-gray-300 px-2 py-1 bg-transparent focus:outline-none pr-1 relative z-10"
+        className="w-[100%] b border-b border-gray-300 px-2 py-1 bg-transparent focus:outline-none pr-1 relative z-10"
       />
     </div>
 
@@ -323,7 +323,7 @@ useEffect(() => {
           const time = e.target.value;
           update('time', time);
         }}
-        className="w-full border-b border-gray-300 px-2 py-1 bg-transparent focus:outline-none pr-1 relative z-10"
+        className="w-[100%] b border-b border-gray-300 px-2 py-1 bg-transparent focus:outline-none pr-1 relative z-10"
       />
     </div>
 
